8bfda92e2395942e5d4a4eea84ebace2e22edc44
zilinghan <zl52@illinois.edu>
2024-01-22 14:05:32 -0600

    update and clean the retriever

--- run_retriever.py
+++ run_retriever.py
@@ -1,34 +1,28 @@
-import json
 import os
+import copy
+import json
+import torch
 import logging
 import argparse
-import torch
-from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.vectorstores import Chroma
+from langchain.embeddings import HuggingFaceInstructEmbeddings
 
 from constants import (
     EMBEDDING_MODEL_NAME,
     PERSIST_DIRECTORY,
     CHROMA_SETTINGS
 )
-def retrieval_DB(device_type):
-    embeddings = HuggingFaceInstructEmbeddings(model_name=EMBEDDING_MODEL_NAME, model_kwargs={"device": device_type})
+def retrieval_DB(device):
+    embeddings = HuggingFaceInstructEmbeddings(model_name=EMBEDDING_MODEL_NAME, model_kwargs={"device": device})
     # This one create Chroma Client and pass it to LangChain
     db = Chroma(
         persist_directory=PERSIST_DIRECTORY,
         embedding_function=embeddings,
         client_settings=CHROMA_SETTINGS
     )
-
-    # This one is basic one. Just load Chroma database
-    # db = Chroma(
-    #     persist_directory=PERSIST_DIRECTORY,
-    #     embedding_function=embeddings,
-    # )
-
     return db
 
-def add_context_to_json(source_dir, target_dir, retriever):
+def add_context_to_json(source_dir: str, target_dir: str, retriever: Chroma, k: int=1):
     if not os.path.exists(target_dir):
         os.makedirs(target_dir)
 
@@ -39,56 +33,34 @@ def add_context_to_json(source_dir, target_dir, retriever):
 
             with open(source_file, 'r') as file:
                 try:
-                    data = json.load(file)
-
-                    docs = retriever.similarity_search(data["Q"])
-                    if len(docs) < 1:
-                        print("There is no relavant context for source file: " + str(source_file))
-                        continue
-                    context = docs[0].page_content
-                    data['Context'] = context
+                    data_origin = json.load(file)
+                    if not isinstance(data_origin, list):
+                        data_origin = [data_origin]
+                    data_augment = []
+                    for data in data_origin:
+                        docs = retriever.similarity_search(data["Q"])
+                        if len(docs):
+                            for doc in docs[:min(k, len(docs))]:
+                                data_cp = copy.copy(data)
+                                data_cp['Context'] = doc.page_content
+                                data_augment.append(data_cp)
                 except Exception as e:
                     print(f"An error occurred during the similarity search: {e}")
 
             with open(target_file, 'w') as file:
-                json.dump(data, file, indent=4)
+                json.dump(data_augment, file, indent=4)
 
 if __name__ == "__main__":
-    logging.basicConfig(
-        format="%(asctime)s - %(levelname)s - %(filename)s:%(lineno)s - %(message)s", level=logging.INFO
-    )
+    logging.basicConfig(format="[%(asctime)s %(levelname)-4s]: %(message)s", level=logging.INFO)
 
     parser = argparse.ArgumentParser(description='Document Processing')
-    parser.add_argument('--device_type', type=str, default='cuda' if torch.cuda.is_available() else 'cpu',
-                        choices=['cpu', 'cuda', 'ipu', 'xpu', 'mkldnn', 'opengl', 'opencl', 'ideep', 'hip', 've', 'fpga', 'ort', 'xla', 'lazy', 'vulkan', 'mps', 'meta', 'hpu', 'mtia'],
-                        help='Device to run on. (Default is cuda)')
+    parser.add_argument('--device', type=str, default='cuda' if torch.cuda.is_available() else 'cpu')
+    parser.add_argument('--num_context', type=int, default=2, help="number of retrieved contexts for each query.")
     args = parser.parse_args()
     ROOT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
     SOURCE_DIR = f"{ROOT_DIRECTORY}/data/datasets/raw"
     TARGET_DIR = f"{ROOT_DIRECTORY}/data/datasets/rag"
     #Test code below
-    db = retrieval_DB(args.device_type)
-    add_context_to_json(SOURCE_DIR, TARGET_DIR, db)
-
-
-    # query = "A-DIT: RETRIEVAL-AUGMENTED DUAL INSTRUCTION TUNING"
-    # answer = "True"
-    # top_k = 1 
-
-    # try:
-    #     # docs = db.similarity_search_with_score(query)
-    #     docs = db.similarity_search(query)
-    #     lgt = len(docs)
-    #     if top_k > lgt:
-    #         print("The num of existing similar doc is less than top_k")
-    #         top_k = lgt
-    #     for i in range(top_k):
-    #         doc = docs[i]
-    #         context = doc.page_content
-    #         # print(doc)
-    #         # print(doc.page_content)
-    #         # print("--------------------")
-    # except Exception as e:
-    #     print(f"An error occurred during the similarity search: {e}")
-
+    db = retrieval_DB(args.device)
+    add_context_to_json(SOURCE_DIR, TARGET_DIR, retriever=db, k=args.num_context)
