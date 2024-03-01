679c85fe519bda7fffdbf9e3b891cf238aa86fe7
5inghalp <38046970+5inghalp@users.noreply.github.com>
2024-03-01 13:35:00 -0500

    updated webscraping script

--- data/docs/pmc_pdf_download_script.py
+++ data/docs/pmc_pdf_download_script.py
@@ -1,74 +1,67 @@
-from urllib.parse import urlparse  # Import urlparse for handling URLs
-from requests_html import HTMLSession  # Import HTMLSession for making HTML requests
+from urllib.parse import urlparse
+from requests_html import HTMLSession
 import requests
-from requests.exceptions import ConnectionError  # Import ConnectionError to handle connection errors
-import os  # Import os for file and directory operations
-import time  # Import time for timing and delays
-import random  # Import random for generating random numbers
+from requests.exceptions import ConnectionError
+import os
+import time
+import random
 
-s = HTMLSession()  # Create an HTML session
+s = HTMLSession()
 
-# Set up headers to mimic a browser request
 headers = {
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
 }
 
-# Create the 'SOURCE_DOCUMENTS' directory if it doesn't exist
 output_dir = 'SOURCE_DOCUMENTS'
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-# Specify the range of lines to read from the input file (1-based indexing)
-start_line = 1
-end_line = 100
+start_line = 58
+end_line = 500
 
-# Open the file containing PMC IDs and read the specified range of lines
 with open('PMC_IDs.txt', 'r') as file:
     ids = [line.strip() for line in file.readlines()[start_line - 1:end_line]]
 
-start_time = time.time()  # Start the timer for total download time
+start_time = time.time()
 
-request_count = 0  # Initialize a counter for the number of requests
+request_count = 0
 
-# Open a log file for writing download messages
-with open('download_log.txt', 'w') as log:
-    # Iterate over each PMC ID
+with open('download_logfile.txt', 'w') as log:
     for pmc in ids:
-        download_start_time = time.time()  # Start the timer for individual download
+        download_start_time = time.time()
         try:
             pmcid = pmc
             base_url = 'https://www.ncbi.nlm.nih.gov/pmc/articles/'
-            # Get the article page
             r = s.get(base_url + pmcid + '/', headers=headers, timeout=5)
-            # Find the PDF link on the article page and construct the PDF URL
-            pdf_url = 'https://www.ncbi.nlm.nih.gov' + r.html.find('a.int-view', first=True).attrs['href']
-            # Download the PDF in chunks and save it to the output directory
-            r = s.get(pdf_url, stream=True)
-            pdf_filename = os.path.join(output_dir, pmcid + '.pdf')
-            with open(pdf_filename, 'wb') as f:
-                for chunk in r.iter_content(chunk_size=1024):
-                    if chunk:
-                        f.write(chunk)
-            download_end_time = time.time()  # Stop the timer for individual download
-            download_time = download_end_time - download_start_time  # Calculate individual download time
-            # Write success message to log and print it
-            log.write(f'Successfully downloaded {pmcid}.pdf in {download_time:.2f} seconds\n')
-            print(f'Successfully downloaded {pmcid}.pdf in {download_time:.2f} seconds')
+            pdf_link = r.html.find('a.int-view', first=True)
+            if pdf_link:
+                pdf_url = 'https://www.ncbi.nlm.nih.gov' + pdf_link.attrs['href']
+                r = s.get(pdf_url, stream=True)
+                pdf_filename = os.path.join(output_dir, pmcid + '.pdf')
+                with open(pdf_filename, 'wb') as f:
+                    for chunk in r.iter_content(chunk_size=1024):
+                        if chunk:
+                            f.write(chunk)
+                download_end_time = time.time()
+                download_time = download_end_time - download_start_time
+                log.write(f'Successfully downloaded {pmcid}.pdf in {download_time:.2f} seconds\n')
+                print(f'Successfully downloaded {pmcid}.pdf in {download_time:.2f} seconds')
+            else:
+                log.write(f'Failed to find PDF link for {pmcid}\n')
+                print(f'Failed to find PDF link for {pmcid}')
 
         except ConnectionError as e:
-            # Write failure message to log and print it
             log.write(f'Failed to download {pmcid}.pdf\n')
             print(f'Failed to download {pmcid}.pdf')
 
-        time.sleep(5)  # Wait for 5 seconds between each request
+        time.sleep(5)
 
-        request_count += 1  # Increment the request counter
-        if request_count % 15 == 0:  # Check if 15 requests have been made
-            random_delay = random.randint(5, 15)  # Generate a random delay between 5 to 15 seconds
-            # Print the duration of the random delay
+        request_count += 1
+        if request_count % 5 == 0:
+            random_delay = random.randint(5, 15)
             print(f'Pausing for {random_delay} seconds...')
-            time.sleep(random_delay)  # Wait for the randomly generated delay before continuing
+            time.sleep(random_delay)
 
-end_time = time.time()  # End the timer for total download time
+end_time = time.time()
 total_time = end_time - start_time
-print(f'Total time taken for downloads: {total_time} seconds')  # Print the total time taken for downloads
+print(f'Total time taken for downloads: {total_time} seconds')
